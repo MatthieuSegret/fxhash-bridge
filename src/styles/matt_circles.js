b85a7e46a2a25c1d2d5b039cb6c72d916e21ac1d
Matthieu Segret <login@matthieusegret.com>
2022-02-18 11:50:53 +0100

    Draw tiles

--- src/styles/matt_circles.js
+++ src/styles/matt_circles.js
@@ -12,37 +12,52 @@ export default class MattCirclesStyle extends Style {
     super(gridSizeX, gridSizeY, s, projectionCalculator3d, p5)
     this.refSize = this._s * 0.04
     this.bgColors = ['#03045E', '#02316F', '#012C65', '#00466B']
+    this.tilesColors = ['#212529', '#343a40', '#495057', '#6c757d']
+
+    this.tilesMask = this._p5.createGraphics(s, s)
+    this.tilesTexture = this._p5.createGraphics(s, s)
   }
 
   beforeDraw () {
     this.background(this.bgColors)
+    this.background(this.tilesColors, this.tilesTexture)
+
+    // Initialize tiles graphics
+    this.tilesMask.background(this._p5.color(255, 255, 255, 0))
+
+    const pixelDensity = this._p5.pixelDensity()
+    this.tilesMask.scale(1 / pixelDensity, 1 / pixelDensity)
   }
 
   drawTile (tilePoints, frontLeftCorner3DCoord, isBorder) {
-    // this._p5.stroke(this.defaultColor)
-    // this._p5.fill(isBorder ? this.colors[2] : this.defaultColor)
-    // this._p5.quad(tilePoints[0].x * this._s, tilePoints[0].y * this._s, tilePoints[1].x * this._s, tilePoints[1].y * this._s, tilePoints[2].x * this._s, tilePoints[2].y * this._s, tilePoints[3].x * this._s, tilePoints[3].y * this._s)
+    this.tilesMask.push()
+    this.tilesMask.fill('#000000')
+    this.tilesMask.quad(tilePoints[0].x * this._s, tilePoints[0].y * this._s, tilePoints[1].x * this._s, tilePoints[1].y * this._s, tilePoints[2].x * this._s, tilePoints[2].y * this._s, tilePoints[3].x * this._s, tilePoints[3].y * this._s)
+    this.tilesMask.pop()
   }
 
   afterDraw () {
-    // this._p5.stroke(this.colors[0])
-    // this._p5.strokeWeight(0.05 * this._s)
-    // this._p5.noFill()
-    // this._p5.rect(0, 0, this._s, this._s)
+    const tilesMaskImg = this.toImage(this.tilesMask, 2)
+    const tilesImg = this.toImage(this.tilesTexture, 2)
+    tilesImg.mask(tilesMaskImg)
+    this._p5.image(tilesImg, 0, 0, this.s, this.s)
   }
 
-  background (colors) {
+  background (colors, graphics) {
+    const g = (graphics === undefined) ? this._p5 : graphics
     const rs = this.refSize
     const size = rs * 0.25
     const weight = rs * 0.12
+    const pixelDensity = this._p5.pixelDensity()
 
-    this._p5.push()
-    this._p5.background(this.bgColors[0])
-    this._p5.noFill()
-    this._p5.strokeWeight(weight)
+    g.push()
+    if (graphics !== undefined) g.scale(1 / pixelDensity, 1 / pixelDensity)
+    g.background(this.bgColors[0])
+    g.noFill()
+    g.strokeWeight(weight)
 
     // Setting shadow
-    const dc = this._p5.drawingContext
+    const dc = g.drawingContext
     dc.shadowOffsetX = rs * 0.03
     dc.shadowOffsetY = rs * 0.03
     dc.shadowColor = '#000'
@@ -53,21 +68,27 @@ export default class MattCirclesStyle extends Style {
       x = 0
       y += size
       for (let j = 0; j < 160; j++) {
-        // intermingles line1 and line2
         const line = [new Vector(x, y - size), new Vector(x - size, y)]
         y = (j % 2 === 0) ? y + size / 6 : y - size / 6
         x -= 0.8 * weight
 
         for (let k = 0; k < 2; k++) {
-          const color = this._p5.random(this.bgColors)
-          this._p5.stroke(color)
+          const color = g.random(colors)
+          g.stroke(color)
           x += 1.5 * weight
           y = (k % 2 === 0) ? y + size / 10 : y - size / 10
-          this._p5.line(line[0].x, line[0].y, line[1].x, line[1].y)
+          g.line(line[0].x, line[0].y, line[1].x, line[1].y)
         }
       }
     }
-    this._p5.pop()
+    g.pop()
+  }
+
+  toImage (g, density) {
+    density ||= g.pixelDensity()
+    const img = this._p5.createImage(g.width * density, g.height * density)
+    img.copy(g, 0, 0, g.width, g.height, 0, 0, g.width * density, g.height * density)
+    return img
   }
 
   static author () {
